4b5ca329ccccb481b7379a539f8dfdf225dc736f
LittleSixfie <jorge-padron99@hotmail.com>
2023-09-17 19:07:20 +0200

    Reset history

---
+++ README.md
@@ -0,0 +1,7 @@
+### Description
+
+A web template I did customaze and host. Also automate the deploy with github actions in orther to learn.
+
+### [Web Live](https://littlesixfie.online)
+
+Template used is this <https://github.com/ubaimutl/react-portfolio>. 
---
+++ src/content_option.js
@@ -0,0 +1,105 @@
+const logotext = "Jorge Padrón";
+const meta = {
+    title: "Jorge Padrón Seisdedos",
+    description: "I’m Jorge software engenieer _ DevOps, currently working in Madrid",
+};
+
+const introdata = {
+    title: "I’m Jorge",
+    animated: {
+        first: "I love cloud",
+        second: "I help developers",
+        third: "I love hardware",
+    },
+    description: "I am a very extrovert person, open-minded and always willing to learn new technologies, or stacks. Right know working as a DevOps engenieer.",
+    your_img_url: "https://images.pexels.com/photos/9553905/pexels-photo-9553905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I am a very extrovert person, open-minded and always willing to learn new technologies, or stacks. Right know working as a DevOps engenieer and loving cloud technologies.",
+};
+const worktimeline = [{
+        jobtitle: "DevOps Consultant",
+        where: "Devoteam",
+        date: "March 2023",
+    },
+    {
+        jobtitle: "Junior Backend Developer",
+        where: "Advance Digital Experts",
+        date: "July 2022",
+    },
+    {
+        jobtitle: "Intership",
+        where: "Advance Digital Experts",
+        date: "September 2021",
+    },
+];
+
+const skills = [{
+        name: "NodeJS",
+        value: 80,
+    },
+    {
+        name: "Bash",
+        value: 75,
+    },
+    {
+        name: "Amazon Web Services",
+        value: 70,
+    },
+    {
+        name: "Jenkins",
+        value: 80,
+    },
+    {
+        name: "Android Studio",
+        value: 65,
+    },
+];
+
+
+const dataportfolio = [{
+        img: "https://www.minecraft.net/content/dam/games/minecraft/key-art/Games_Subnav_Minecraft-300x465.jpg",
+        description: "Cloudformation template to deploy minecraft server with a launch script",
+        link: "https://github.com/LittleSixfie/Minecraft-CloudFormation",
+    },
+    {
+        img: "https://reportportal.io/resources/item5.ce6084.jpg",
+        description: "Changes made for an agent of a open source project.",
+        link: "https://github.com/LittleSixfie/agent-swift-XCTest",
+    },
+    {
+        img: "https://vigueras.dev/wp-content/uploads/2020/09/github-actions.jpg",
+        description: "The automation of this website on a server with github actions.",
+        link: "https://github.com/LittleSixfie/stolenfolio",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "not-a-real-mail@hotmail.com",
+    
+    description: "If you want to contact with for any mather use my linkedIn to reach me.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/LittleSixfie",
+    linkedin: "https://www.linkedin.com/in/jorge-padron-seisdedos-76ba9846",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
